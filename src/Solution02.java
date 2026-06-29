003f8f2a4f88187354e2b0c4307c533c43b0c168
a1l1ke <onetenthplease@gmail.com>
2026-06-29 10:35:18 +0900

    Solution02 ReentrantLock 예제(LockCounter) 및 synchronized 예제 구현

--- src/Solution02.java
+++ src/Solution02.java
@@ -1,6 +1,26 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Solution02 {
     public static void main(String[] args) throws InterruptedException {
-        runSync();
+//        runSync();
+        runLock();
+    }
+
+    static void runLock() throws InterruptedException {
+        LockCounter counter = new LockCounter();
+        Runnable task = () -> {
+            for (int i = 0; i < 50_000; i++) {
+                counter.increment();
+            }
+        };
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("counter.getCount() = " + counter.getCount());
     }
 
     static void runSync() throws InterruptedException {
@@ -8,28 +28,48 @@ public class Solution02 {
         Runnable task = () -> {
             for (int i = 0; i < 50_000; i++) {
                 counter.increment();
-                try {
-                    Thread.sleep(Math.round(Math.random()));
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
             }
         };
         Thread thread1 = new Thread(task);
         Thread thread2 = new Thread(task);
         thread1.start();
+        thread2.start(); // join -> 스레드 처리까지 대기
         thread1.join();
-        thread2.start();
         thread2.join();
         System.out.println("counter.getCount() = " + counter.getCount());
+    }
+}
+
+class LockCounter {
+    private int count = 0;
+    private final Lock lock = new ReentrantLock();
+
+    public void increment() {
+        lock.lock(); // 락을 획득하고, 획득할 때 대기
+        try {
+            count++;
+            if (count % 10000 == 0) {
+                System.out.println(Thread.currentThread().getName() + " / count = " + count);
+            }
+        } finally {
+            lock.unlock(); // 무조건 언락해야함 -> 안하면 deadlock
+        }
+    }
+
+    public int getCount() {
+        return count;
     }
 }
 
 class SyncCounter {
     private int count = 0;
 
-    public void increment() {
+    //    public void increment() {
+    public synchronized void increment() {
         count++;
+        if (count % 10000 == 0) {
+            System.out.println(Thread.currentThread().getName() + " / count = " + count);
+        }
     }
 
     public int getCount() {
