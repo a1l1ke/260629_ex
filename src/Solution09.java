38a206a653bb3f033edf2bb4aa772d02841bdd4c
a1l1ke <onetenthplease@gmail.com>
2026-06-29 15:18:15 +0900

    feat: add Solution09 skeletal file for parallel streams

---
+++ src/Solution09.java
@@ -0,0 +1,10 @@
+public class Solution09 {
+    public static void main(String[] args) {
+        // stream, parallel
+        runStream();
+    }
+
+    static void runStream() {
+        
+    }
+}
