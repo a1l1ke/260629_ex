719d9e710f3b5041d9f7eb108afe8f3aaa15620a
a1l1ke <onetenthplease@gmail.com>
2026-06-29 15:05:03 +0900

    feat: add Solution08 skeletal file with names list

---
+++ src/Solution08.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Solution08 {
+    static List<String> names = List.of("Kim", "Park", "Lee", "Choi", "Jung");
+
+    public static void main(String[] args) {
+
+    }
+}
