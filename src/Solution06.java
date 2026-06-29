0d2e01197f074d00005c18d196dfdbdb48949939
a1l1ke <onetenthplease@gmail.com>
2026-06-29 12:33:29 +0900

    Solution06 함수형 인터페이스 및 익명 클래스 예제 구현

--- src/Solution06.java
+++ src/Solution06.java
@@ -1,2 +1,20 @@
 public class Solution06 {
+    public static void main(String[] args) {
+        runOld();
+    }
+
+    static void runOld() {
+        CustomPrinter printer = new CustomPrinter() {
+            @Override
+            public void print(String msg) {
+                System.out.println("익명 클래스에서 %s".formatted(msg));
+            }
+        };
+        printer.print("Hello");
+    }
+}
+
+@FunctionalInterface // implements하지 않아도 해당 메서드에 대한 구현을 익명함수로 직접해도 인스턴화 가능
+interface CustomPrinter {
+    void print(String msg);
 }
